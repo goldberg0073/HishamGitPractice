229d46a897bf599987af7b76379e2adb45b4bc8f
goldberg0073 <hicham_goldberg@hotmail.com>
2020-11-27 20:37:24 -0500

    commit master branch

--- src/test/java/logout.java
+++ src/test/java/logout.java
@@ -1,5 +1,7 @@
 public class logout {
     public static void main(String[] args) {
-        System.out.println("logout");
+
+        System.out.println("this is happening in the master branch");
+
     }
 }
