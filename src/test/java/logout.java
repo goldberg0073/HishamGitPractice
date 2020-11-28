35709d89f7c965314b7225878d8e8bf6aca0c451
goldberg0073 <hicham_goldberg@hotmail.com>
2020-11-27 20:39:22 -0500

    commit to login branch

--- src/test/java/logout.java
+++ src/test/java/logout.java
@@ -1,7 +1,8 @@
 public class logout {
     public static void main(String[] args) {
-        System.out.println("logout");
-        System.out.println("see you in intelleji");
+
+
+        System.out.println("this is happening in the login branch");
         
     }
 }
